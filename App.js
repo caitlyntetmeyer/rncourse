779086cf69098a6801b6b3deb522bfedb6f64ccf
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-01-29 11:45:47 -0500

    Commented out unnecessary code

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { StyleSheet, View } from 'react-native';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 export default class App extends Component {
   state = {
