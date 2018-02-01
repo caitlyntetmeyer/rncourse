f67f286679e49767a75cea808edb7376a8ccef6d
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-02-01 11:52:12 -0500

    Added ScrollView to enable scrolling if list gets long

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
